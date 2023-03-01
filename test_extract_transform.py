bde23f5f9fd1abcb933a4443b50e76c165615d8e
Eakamon Aunchalanon <eakamon@thinknet.co.th>
2023-03-01 17:53:02 +0700

    first commit

---
+++ test_extract_transform.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+
+default_args = {
+    'owner': 'eakamon',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='test_etl_dag_v01',
+    default_args=default_args,
+    start_date=datetime(2023, 3, 1),
+    schedule_interval='@daily'
+) as dag:
+    task1 = KubernetesPodOperator(
+        namespace="airflow-cluster",
+        image="registry.thinknet.co.th/big-data/extract-from-s3:1.1.0",
+        image_pull_secrets=[k8s.V1LocalObjectReference("regcred")],
+        # cmds=["bash", "-cx"],
+        # arguments=["echo", "10", "echo pwd"],
+        labels={"foo": "bar"},
+        name="test_etl_extract",
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="task-one-extract",
+        get_logs=True,
+    )
+
+    task2 = KubernetesPodOperator(
+        namespace="airflow-cluster",
+        image="registry.thinknet.co.th/big-data/transform:2.0.0",
+        image_pull_secrets=[k8s.V1LocalObjectReference("regcred")],
+        # cmds=["bash", "-cx"],
+        # arguments=["echo", "10", "echo pwd"],
+        labels={"foo": "bar"},
+        name="test_etl_transform",
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="task-two-transform",
+        get_logs=True,
+    )
+    
+    task1 >> task2
